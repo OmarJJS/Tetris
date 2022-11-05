56d3e95a26ed16d10661b29a1ef4995b33f7e7b0
Omar Jimenez <jojimenezs98@gmail.com>
2022-11-04 19:36:22 -0600

    Initial Commit

---
+++ Swiftris/Game/ViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// this view controller not used.
+// entry view controller is SwiftrisViewController.
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override var prefersStatusBarHidden : Bool {
+        return false
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        let swiftrisViewController = SwiftrisViewController()
+        self.present(swiftrisViewController, animated: true, completion: nil)
+    }
+}
+
